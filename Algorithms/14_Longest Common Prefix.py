9d07959d09a790e5473afd094af883e669af555d
Damnsure <bing900713@163.com>
2017-09-28 17:46:43 +0800

    Problem sets updated

---
+++ Algorithms/14_Longest Common Prefix.py
@@ -0,0 +1,13 @@
+__author__ = 'nickyuan'
+'''
+Write a function to find the longest common prefix string amongst an array of strings.
+'''
+
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        
